083e072fe54b0a40f3dc0635055c316d85ad19a4
Angorosh <alighieri920@gmail.com>
2023-04-06 22:28:56 +0300

    Add new classes

---
+++ src/main/java/org/study/firstTheme.java
@@ -0,0 +1,16 @@
+package org.study;
+
+public class firstTheme {
+    public static void Variables_And_Types() {
+        System.out.println("Тема 1: Переменные и типы данных.");
+
+        int Age = 77;
+        System.out.println("Возраст: " + Age);
+
+        float Num_1 = 4.5345f;
+        System.out.println(Num_1);
+
+        double Num_2 = 4.124124234234;
+        System.out.println(Num_2);
+    }
+}
